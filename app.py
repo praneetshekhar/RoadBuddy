f9b81610bd70d35d6804287b9a284b488f1e7a40
Praneet Shekhar <praneetshekhar@Praneets-MacBook-Air.local>
2021-11-23 06:23:04 +0530

    added wsgi support, added Procfile

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
-def hello_world():
-    return 'Hello Cloud!'
+@app.route('/')
+def index():
+    return 'Hello Cloud!' 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
